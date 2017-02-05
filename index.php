7a4d405ceacbc1cd1f6388707270f3caef0f2e77
RawIron <manianology@gmail.com>
2017-02-05 20:47:09 +0100

    move settings into di, based on hostname decide which settings to set.

--- index.php
+++ index.php
@@ -6,6 +6,32 @@ use Phalcon\Di\FactoryDefault;
 use Phalcon\Db\Adapter\Pdo\Mysql as PdoMysql;
 
 
+$settings = [];
+
+if (gethostname() == "vagrant") {
+    $settings = [
+        "mysql" => [
+                "host"     => "127.0.0.1",
+                "port"     => 3306,
+                "username" => "admin",
+                "password" => "similarly-secure-password",
+                "dbname"   => "customers"
+                ]
+        ];
+}
+else {
+    $settings = [
+        "mysql" => [
+                "host"     => "172.17.0.5",
+                "port"     => 3306,
+                "username" => "admin",
+                "password" => "similarly-secure-password",
+                "dbname"   => "customers"
+                ]
+        ];
+}
+
+
 // Use Loader() to autoload our model
 $loader = new Loader();
 
@@ -19,16 +45,14 @@ $loader->registerDirs(
 // Inject Dependencies
 $di = new FactoryDefault();
 
+// it is no possible to set config to the settings array directly
+// and Phalcon\\Config cannot be passed into Pdo\Mysql
+$di->set('config', function() use ($settings) { return $settings; });
+
 // Set up the database service
 $di->set('db', function () {
-    return new PdoMysql(
-        array(
-            "host"     => "localhost",
-            "username" => "admin",
-            "password" => "similarly-secure-password",
-            "dbname"   => "customers"
-        )
-    );
+    $config = $this->get("config");
+    return new PdoMysql($config["mysql"]);
 });
 
 
